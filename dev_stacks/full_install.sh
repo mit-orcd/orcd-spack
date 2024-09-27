99d37982c30f341791a7b4298a2e125e3ea5be3b
Lauren Milechin <milechin@mit.edu>
2024-09-27 14:41:54 -0400

    fix env names

--- dev_stacks/full_install.sh
+++ dev_stacks/full_install.sh
@@ -17,20 +17,20 @@ source $SPACK_INSTALL/share/spack/setup-env.sh
 export SPACK_DISABLE_LOCAL_CONFIG=true
 
 # Base environment has only compilers installed, has same install_tree root as core_stack_env
-spack env activate ./base_stack_env 
+spack env activate ./base_stack 
 spack install
 GCC_LOC=$(spack location -i gcc)
 spack env deactivate
 
 # Core software environment has compilers + core software stack
-spack env activate ./core_stack_env 
+spack env activate ./core_stack
 spack compiler find $GCC_LOC # add compilers installed from base_stack_env
 spack install # install the rest of the core software
 spack module lmod refresh --delete-tree -y
 spack env deactivate
 
 # Community stack has install root for base+core as upstream
-spack env activate ./community_stack_env
+spack env activate ./community_stack
 spack compiler find $GCC_LOC
 spack install
 spack module lmod refresh --delete-tree -y
